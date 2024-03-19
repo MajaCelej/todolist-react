90b52770e319a11d1ee206bca912687818d32ee1
Maja Celej <cmaja1825@gmail.com>
2024-03-19 09:52:51 +0100

    created TaskPage

---
+++ src/features/tasks/TaskPage/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Section from "../../../common/Section";
+import Header from "../../../common/Header";
+import Main from "../../../common/Main";
+
+function TaskPage() {
+	return (
+		<Main>
+			<Header title="Szczegóły zadania" />
+			<Section
+                title={""}
+                body={<>nana</>} 
+            />
+		</Main>
+	);
+}
+
+export default TaskPage;
