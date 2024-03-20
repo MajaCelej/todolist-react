ab32c5897ed27a78356318181742f283def7f2b3
Maja Celej <cmaja1825@gmail.com>
2024-03-20 19:33:12 +0100

    changed body and title in Section; added useSelector and useParams

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -2,14 +2,22 @@ import React from "react";
 import Section from "../../../common/Section";
 import Header from "../../../common/Header";
 import Main from "../../../common/Main";
+import { useParams } from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { getTaskById } from "../tasksSlice";
 
 function TaskPage() {
+	const { id } = useParams();
+	const task = useSelector(state => getTaskById(state, id));
+
 	return (
 		<Main>
 			<Header title="Szczegóły zadania" />
 			<Section
-                title={""}
-                body={<>nana</>} 
+                title={task ? task.content : "Nie znaleniono zadania"}
+                body={
+					<><strong>Ukończono:</strong> {task.done ? "Tak" : "Nie"}</>
+				} 
             />
 		</Main>
 	);
