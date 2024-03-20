d0d00e779c809bb7491d7a9ad106066cd97caffb
Maja Celej <cmaja1825@gmail.com>
2024-03-20 19:57:54 +0100

    added const location and const query; imported useLocation

--- src/features/tasks/TasksPage/TaskList/index.js
+++ src/features/tasks/TasksPage/TaskList/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { List, Item, Span, Content, Button } from "./styled";
-import { selectTasks, toggleTaskDone, removeTask, selectHideDone } from "../../tasksSlice";
+import { toggleTaskDone, removeTask, selectHideDone, selectTasksByQuery } from "../../tasksSlice";
 
 const TaskList = () => {
-    const tasks = useSelector(selectTasks);
+	const location = useLocation();
+	const query = (new URLSearchParams(location.search)).get("szukaj");  
+
+    const tasks = useSelector(state => selectTasksByQuery(state, query));
 	const hideDone = useSelector(selectHideDone);
 
 	const dispatch = useDispatch();
