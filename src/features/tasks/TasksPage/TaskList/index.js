ae4d4490a6abe01053a82a31aa5171fba5025adb
Maja Celej <cmaja1825@gmail.com>
2024-03-21 09:58:27 +0100

    added query and imported searchQueryParamName

--- src/features/tasks/TasksPage/TaskList/index.js
+++ src/features/tasks/TasksPage/TaskList/index.js
@@ -3,10 +3,11 @@ import { useSelector, useDispatch } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 import { List, Item, Span, Content, Button } from "./styled";
 import { toggleTaskDone, removeTask, selectHideDone, selectTasksByQuery } from "../../tasksSlice";
+import searchQueryParamName from "../searchQueryParamName";
 
 const TaskList = () => {
 	const location = useLocation();
-	const query = (new URLSearchParams(location.search)).get("szukaj");  
+	const query = (new URLSearchParams(location.search)).get(searchQueryParamName);  
 
     const tasks = useSelector(state => selectTasksByQuery(state, query));
 	const hideDone = useSelector(selectHideDone);
