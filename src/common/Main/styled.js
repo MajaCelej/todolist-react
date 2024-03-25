524d2b46a02d345da8053a363c95cf56f7fdb901
Maja Celej <cmaja1825@gmail.com>
2024-03-25 08:31:28 +0100

    added bottom-margin

--- src/common/Main/styled.js
+++ src/common/Main/styled.js
@@ -4,6 +4,7 @@ export const Wrapper = styled.main`
   display: grid;
   justify-content: center;
   grid-gap: 20px;
+  margin-bottom: 40px;
 
   @media (max-width: 700px) {
     grid-template-columns: 350px;
