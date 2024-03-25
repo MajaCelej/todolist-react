6503219c4c7505e22debb8d56276ecec1a46fc00
Maja Celej <cmaja1825@gmail.com>
2024-03-25 08:34:15 +0100

    minor corrections

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -2,23 +2,27 @@ import React from "react";
 import Section from "../../../common/Section";
 import Header from "../../../common/Header";
 import Main from "../../../common/Main";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { getTaskById } from "../tasksSlice";
 
 function TaskPage() {
 	const { id } = useParams();
-	const task = useSelector(state => getTaskById(state, id));
+	const task = useSelector((state) => getTaskById(state, id));
 
 	return (
 		<Main>
 			<Header title="Szczegóły zadania" />
 			<Section
-                title={task ? task.content : "Nie znaleniono zadania"}
-                body={
-					<><strong>Ukończono:</strong> {task.done ? "Tak" : "Nie"}</>
-				} 
-            />
+				title={task ? task.content : "Nie znaleźliśmy takiego zadania"}
+				body={
+					!!task && (
+						<>
+							<strong>Ukończono:</strong> {task.done ? "Tak" : "Nie"}
+						</>
+					)
+				}
+			/>
 		</Main>
 	);
 }
