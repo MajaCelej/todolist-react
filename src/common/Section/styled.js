18819d963ddec8c9fded8eb84edd0a42550436ca
Maja Celej <cmaja1825@gmail.com>
2024-03-27 10:42:26 +0100

    chaned styles in Wrapper

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -1,27 +1,26 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.section`
-  background-color: ${({ theme }) => theme.color.white};
-  width: 800px;
-
-  @media (max-width: 992px) {
-      width: 500px;
-    }
+	background-color: ${({ theme }) => theme.color.white};
+	display: flex;
+	flex-direction: column;
+	gap: 2px;
+	margin-bottom: 10px;
 `;
 
 export const SectionHeader = styled.div`
-  border-bottom: 2px solid ${({ theme }) => theme.color.mercury};
-  padding: 5px;
-  margin: 0px 20px;
-  display: flex;
-  justify-content: space-between;
+	border-bottom: 2px solid ${({ theme }) => theme.color.mercury};
+	padding: 5px;
+	margin: 0px 20px;
+	display: flex;
+	justify-content: space-between;
 
-    @media (max-width: 992px) {
-      display: grid;
-      grid-template-columns: 1fr;
-    }
+	@media (max-width: 992px) {
+		display: grid;
+		grid-template-columns: 1fr;
+	}
 `;
 
 export const SectionBody = styled.div`
-  padding: 15px 25px;
+	padding: 15px 25px;
 `;
