ce7aa6082b99874a366d5bbf44b01d8e6ab03fcf
Maja Celej <cmaja1825@gmail.com>
2024-03-27 10:42:44 +0100

    changed styles in input

--- src/features/tasks/TasksPage/Input/index.js
+++ src/features/tasks/TasksPage/Input/index.js
@@ -1,16 +1,8 @@
 import styled from "styled-components";
 
 export default styled.input`
-  border: 1px solid ${({ theme }) => theme.color.silver};
-  width: 700px;
-  padding: 10px;
-
-    @media (max-width: 700px) {
-        flex-basis: 100%;
-        margin-bottom: 10px;
-    }
-
-    @media (max-width: 992px) {
-        flex-basis: 100%;
-    }
+    padding: 8px;
+    border: 1px solid ${({ theme }) => theme.color.silver};
+    flex-grow: 1;
+    overflow: auto;
 `;
